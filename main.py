f533059fd28acd8d66bc23e9ce9063eb8821c8d8
Arseny <116337996+CHMArs777@users.noreply.github.com>
2023-03-31 17:58:24 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,46 @@
-# Pygame шаблон - скелет для нового проекта Pygame
-import pygame
-import random
-
-WIDTH = 360
-HEIGHT = 480
-FPS = 30
-# Задаем цвета
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-# Создаем игру и окно
-pygame.init()
-pygame.mixer.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("My Game")
-clock = pygame.time.Clock()
-# Цикл игры
-running = True
-while running:
-    # Держим цикл на правильной скорости
-    clock.tick(FPS)
-    # Ввод процесса (события)
-    for event in pygame.event.get():
-        # check for closing window
-        if event.type == pygame.QUIT:
-            running = False
-
-pygame.quit()
+from random import randint
+import time
+hp = 100
+speed = 0
+distance = 0
+totalDistance = 400
+rock = 1
+yeti = 2
+niger = 3
+def CheckSkill(enemy, hp, speed):
+    print('Влево - 1; Вправо - 2; Подпрыгнуть - 3;')
+    storona = int(input('Куда повернуть?:'))
+    udar = randint(1, 3)
+    if storona == udar:
+        hp = hp - 20
+        speed = 0
+        print('Ты впечатался в препятствие')
+        print('ХП:', hp)
+    elif (storona != 1) and (storona != 2) and (storona != 3):
+        print('Ты сделал неудачную сальтуху')
+        hp = hp - 20
+        speed = 0
+    else:
+        print('Ты увернулся. Красавчик!')
+while True:
+    if hp <= 0:
+        print("ПОТРАЧЕНО")
+        break
+    if distance >= totalDistance:
+        print("ЛЕГЧАЙШЕ")
+        break
+    speed = speed + 1
+    print(speed, " скорость")
+    distance = distance + speed
+    print(distance, " дистанция")
+    time.sleep(0.5)
+    chance = randint(1,3)
+    if chance == yeti:
+        print('Ты попался на ЙЕТИ!')
+        CheckSkill(yeti, hp, speed)
+    if chance == rock:
+        print('Its ROOOOOCK!')
+        CheckSkill(rock, hp, speed)
+    if chance == niger:
+        print('niger niger niger')
+        CheckSkill(yeti, hp, speed)
